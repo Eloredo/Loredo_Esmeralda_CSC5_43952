1446a1ec6cd3cb0a224dfe4221342ff42213b3e6
Eloredo <ezster09@gmail.com>
2015-03-10 00:44:14 -0700

    Signed-off-by: Eloredo <ezster09@gmail.com>

--- HmWk/Assignment_1/Gladdis_7thEd_Chp2_Prob4_ResturantBill/main.cpp
+++ HmWk/Assignment_1/Gladdis_7thEd_Chp2_Prob4_ResturantBill/main.cpp
@@ -1,19 +1,40 @@
-/* 
+/* //Restaurant Bill
  * File:   main.cpp
- * Author: user
+ * Author: Esmeralda Loredo
  *
  * Created on March 10, 2015, 12:01 AM
  */
 
-#include <cstdlib>
+#include <iostream>
 
 using namespace std;
-
-/*
- * 
- */
+//Write a program that computes the tax on a restaurant bill 
+//For a patron with a $44.50 meal charge
+//Tax 6.75 percent
+//Tip 15% of the total after adding the tax
+//Display the meal cost, tax amount, tip, & total bill
+//Execution Begins Here!
 int main(int argc, char** argv) {
-
+    //Declare Variables 
+    int mCost, total, totalWT; 
+    double taxAmt;
+    double tip;
+    double totalB;
+    
+    mCost=44.50;//Meal Charge
+    taxAmt=0.0675;//Percent of Tax
+    tip=0.15;//Percent of tip after adding the tax
+    //Calculate and display transaction 
+    total= mCost*(taxAmt);//Total meal cost w/tax
+    totalWT=mCost*(tip);//Total meal cost w/ tip
+    totalB= mCost+total+totalWT;//Grand total w/ tax + tip
+    
+    cout<< "Meal Cost: $"<<mCost<<endl;
+    cout<< "Tax Amount: $"<<total<<endl;
+    cout<< "Tip: "<<tip<<"%"<<" ="<<6.68<<endl;
+    cout<< "Total Bill: $"<<totalB<<endl;
+   
+    //Exit Stage Right!
     return 0;
 }
 
