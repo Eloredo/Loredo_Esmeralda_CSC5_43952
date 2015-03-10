b630c9b7bdecaa86658fa0ba32d0f82c1bf08292
Eloredo <ezster09@gmail.com>
2015-03-09 20:00:20 -0700

    Signed-off-by: Eloredo <ezster09@gmail.com>

---
+++ HmWk/Assignment_1/Gladdis_7thEd_Chp2_Prob14_PersonalInformation/main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: Esmeralda Loredo
+ *
+ * Created on March 9, 2015, 7:00 PM
+ */
+
+#include <iostream>
+using namespace std;
+
+//Execution Begins Here!
+
+int main(int argc, char** argv) {
+    //Declare variables
+    int name, address, city, state, ZIPCode, major;
+    //Input command line
+    cout<< " Your name\n Your address with city, state, and Zip code\n Your Telephone number\n Your college major";   
+    //Exit Right Door!
+    return 0;
+}
+
